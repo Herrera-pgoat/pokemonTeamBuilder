bfc26e2e9eab79a0dc99b45e3719fa65cddd7990
Herrera-pgoat <levi.herrera91@gmail.com>
2020-03-03 21:29:43 -0600

    Starting pokemon project

---
+++ __init__.py
@@ -0,0 +1,38 @@
+#This is the init py file but I don't  konw what to put in here :(
+import os
+
+#importing flask 
+from flask import Flask,render_template
+
+#Function Name :create_app
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='iorwj380jrir8(U#$jiofj04',
+		#I guess this is where the database will be
+		#plan on switching this to mongoDB
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+		#This is loading it from a python file how cool 
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+		#this is because we don't have a config.py so we are just doing it old school
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return render_template('base.html')
+
+    return app

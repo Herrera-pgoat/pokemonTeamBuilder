eacebcd6a6387d4bc3e3db170ef79c46ca137c0b
Herrera-pgoat <levi.herrera91@gmail.com>
2020-03-16 16:30:41 -0500

    All right have not committed in a minute but that is because of corona stuff and I was thinking about back end and got lost in that thought. It sort of made me lose focus on my project. Going to try to focus on front end stuff for now. Althogh I am looking at redux to keep state but that  maybe added in a future update. Added a table to the main screen and divied up how the main screen will look. The table is also there and is made using react and each element is its own react component down to the square! Added a new css document so that I can do my own styling on certain elements of the page

---
+++ reactJs/partials/_tableType.js
@@ -0,0 +1,88 @@
+//https://clips.twitch.tv/AverageGeniusMinkMingLee leffen likes toradora
+class TableType extends React.Component{
+  constructor(props){
+    super(props);
+    /* Ok right here if I want to do anything with the props they sent in they I might have it interact with the state*/
+  }
+
+  render(){
+    /* Making a table elem thing righ here*/
+    table = (
+      <div   >
+        <TableRow type="0" />
+        <TableRow type="Bug" />
+        <TableRow type="Dark" />
+        <TableRow type="Dragon" />
+        <TableRow type="Electric" />
+        <TableRow type="Fairy" />
+        <TableRow type="Fighting" />
+        <TableRow type="Fire" />
+        <TableRow type="Flying" />
+        <TableRow type="Ghost" />
+        <TableRow type="Grass" />
+        <TableRow type="Ground" />
+        <TableRow type="Ice" />
+        <TableRow type="Normal" />
+        <TableRow type="Poison" />
+        <TableRow type="Psychic" />
+        <TableRow type="Rock" />
+        <TableRow type="Steel" />
+        <TableRow type="Water" />
+      </div>)
+    return (
+      table
+    );/*This is the end of return */
+  }
+}
+
+class TableRow extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    const type = this.props.type;
+    let returnedRow;
+    if ( type != "0") {
+      returnedRow = (<tr class="type" id={this.props.type} > {this.props.type}
+        <TableSquare word="Zero " />
+        <TableSquare word="Point 25" />
+        <TableSquare word="Point 5" />
+        <TableSquare word="Integer 1" />
+        <TableSquare word="Integer 2" />
+        <TableSquare word="Integer 4" />
+      </tr>);
+    }
+    else{
+      returnedRow = (
+        <tr id="RowWeakness"> Strength of Hit
+            <td > 0x </td>
+            <td> 0.25x </td>
+            <td > 0.5x </td>
+            <td> 1x </td>
+            <td > 2x </td>
+            <td> 4x </td>
+        </tr>
+      );
+    }
+    return(
+      returnedRow
+    );
+  }
+}
+
+class TableSquare extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {num : 0};
+  }
+
+  render(){
+    return(
+        <td class="square"> {this.state.num} {this.props.word} </td>
+    );
+  }
+}
+
+
+ ReactDOM.render( <TableType/>,   document.getElementById('table') );
---
+++ templates/partials/_tableType.js
@@ -0,0 +1,155 @@
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+//https://clips.twitch.tv/AverageGeniusMinkMingLee leffen likes toradora
+var TableType = function (_React$Component) {
+  _inherits(TableType, _React$Component);
+
+  function TableType(props) {
+    _classCallCheck(this, TableType);
+
+    return _possibleConstructorReturn(this, (TableType.__proto__ || Object.getPrototypeOf(TableType)).call(this, props));
+    /* Ok right here if I want to do anything with the props they sent in they I might have it interact with the state*/
+  }
+
+  _createClass(TableType, [{
+    key: "render",
+    value: function render() {
+      /* Making a table elem thing righ here*/
+      table = React.createElement(
+        "div",
+        null,
+        React.createElement(TableRow, { type: "0" }),
+        React.createElement(TableRow, { type: "Bug" }),
+        React.createElement(TableRow, { type: "Dark" }),
+        React.createElement(TableRow, { type: "Dragon" }),
+        React.createElement(TableRow, { type: "Electric" }),
+        React.createElement(TableRow, { type: "Fairy" }),
+        React.createElement(TableRow, { type: "Fighting" }),
+        React.createElement(TableRow, { type: "Fire" }),
+        React.createElement(TableRow, { type: "Flying" }),
+        React.createElement(TableRow, { type: "Ghost" }),
+        React.createElement(TableRow, { type: "Grass" }),
+        React.createElement(TableRow, { type: "Ground" }),
+        React.createElement(TableRow, { type: "Ice" }),
+        React.createElement(TableRow, { type: "Normal" }),
+        React.createElement(TableRow, { type: "Poison" }),
+        React.createElement(TableRow, { type: "Psychic" }),
+        React.createElement(TableRow, { type: "Rock" }),
+        React.createElement(TableRow, { type: "Steel" }),
+        React.createElement(TableRow, { type: "Water" })
+      );
+      return table; /*This is the end of return */
+    }
+  }]);
+
+  return TableType;
+}(React.Component);
+
+var TableRow = function (_React$Component2) {
+  _inherits(TableRow, _React$Component2);
+
+  function TableRow(props) {
+    _classCallCheck(this, TableRow);
+
+    return _possibleConstructorReturn(this, (TableRow.__proto__ || Object.getPrototypeOf(TableRow)).call(this, props));
+  }
+
+  _createClass(TableRow, [{
+    key: "render",
+    value: function render() {
+      var type = this.props.type;
+      var returnedRow = void 0;
+      if (type != "0") {
+        returnedRow = React.createElement(
+          "tr",
+          { "class": "type", id: this.props.type },
+          " ",
+          this.props.type,
+          React.createElement(TableSquare, { word: "Zero " }),
+          React.createElement(TableSquare, { word: "Point 25" }),
+          React.createElement(TableSquare, { word: "Point 5" }),
+          React.createElement(TableSquare, { word: "Integer 1" }),
+          React.createElement(TableSquare, { word: "Integer 2" }),
+          React.createElement(TableSquare, { word: "Integer 4" })
+        );
+      } else {
+        returnedRow = React.createElement(
+          "tr",
+          { id: "RowWeakness" },
+          " Strength of Hit",
+          React.createElement(
+            "td",
+            null,
+            " 0x "
+          ),
+          React.createElement(
+            "td",
+            null,
+            " 0.25x "
+          ),
+          React.createElement(
+            "td",
+            null,
+            " 0.5x "
+          ),
+          React.createElement(
+            "td",
+            null,
+            " 1x "
+          ),
+          React.createElement(
+            "td",
+            null,
+            " 2x "
+          ),
+          React.createElement(
+            "td",
+            null,
+            " 4x "
+          )
+        );
+      }
+      return returnedRow;
+    }
+  }]);
+
+  return TableRow;
+}(React.Component);
+
+var TableSquare = function (_React$Component3) {
+  _inherits(TableSquare, _React$Component3);
+
+  function TableSquare(props) {
+    _classCallCheck(this, TableSquare);
+
+    var _this3 = _possibleConstructorReturn(this, (TableSquare.__proto__ || Object.getPrototypeOf(TableSquare)).call(this, props));
+
+    _this3.state = { num: 0 };
+    return _this3;
+  }
+
+  _createClass(TableSquare, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "td",
+        { "class": "square" },
+        " ",
+        this.state.num,
+        " ",
+        this.props.word,
+        " "
+      );
+    }
+  }]);
+
+  return TableSquare;
+}(React.Component);
+
+ReactDOM.render(React.createElement(TableType, null), document.getElementById('table'));
